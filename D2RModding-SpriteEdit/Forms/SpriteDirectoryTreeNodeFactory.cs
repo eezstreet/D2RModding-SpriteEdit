d1158239d2ec5989781c1c8f9b02aabb02dee0d9
Jonathan McKinney <jmlonghorn88@gmail.com>
2022-03-28 17:57:00 -0500

    Can now add directories to treeview on left of application

---
+++ D2RModding-SpriteEdit/Forms/SpriteDirectoryTreeNodeFactory.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D2RModding_SpriteEdit.Forms
+{
+    class SpriteDirectoryTreeNodeFactory
+    {
+        // Key: DirectoryInfo.FullName
+        private Dictionary<string, SpriteDirectoryTreeNode> cache;
+
+        public SpriteDirectoryTreeNodeFactory()
+        {
+            cache = new Dictionary<string, SpriteDirectoryTreeNode>();
+        }
+
+        public SpriteDirectoryTreeNode getNode(DirectoryInfo dirInfo)
+        {
+            if (!cache.ContainsKey(dirInfo.FullName))
+            {
+                var dirTreeNode = new SpriteDirectoryTreeNode(dirInfo);
+                dirTreeNode.Nodes.AddRange(dirInfo.GetFiles("*.sprite").Select((fileInfo) => new FileTreeNode(fileInfo)).ToArray());
+            
+                dirTreeNode.Nodes.AddRange(dirInfo.GetDirectories().Select((subDirInfo) => {
+                    var node = getNode(subDirInfo);
+                    if (node.Parent != null)
+                    {
+                        node.Parent.Nodes.Remove(node);
+                    }
+                    if (node.TreeView != null)
+                    {
+                        node.Remove();
+                    }
+                    return node;
+                }).ToArray());
+
+                cache[dirInfo.FullName] = dirTreeNode;
+            }
+
+            return cache[dirInfo.FullName];
+        }
+    }
+}
